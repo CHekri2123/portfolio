014e95d2c02c59bab1417a7407e8f5c958c75a62
Kalyan <ganjikalyan05@gmail.com>
2024-08-19 01:01:45 +0530

    changed to hashrouter for fiximg routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom"; // Import HashRouter
 import App from "./App";
 import './App.css';
 import './index.css';
